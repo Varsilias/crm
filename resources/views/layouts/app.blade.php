d06dda71fbe69bcc75c1fb5e9247332fb1fb5a14
Daniel Okoronkwo <61119104+danielokoronkwo-coder@users.noreply.github.com>
2021-11-05 20:03:25 +0100

    Fixed hambuger menu and login display image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,14 +18,14 @@
     <link rel="manifest" href="{{ asset('favicon/site.webmanifest')}}">
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
 
